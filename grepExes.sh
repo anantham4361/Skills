21eafbfdeb4c652c5aa6810452ade61293f0b167
anantham4361 <anantham4361@gmail.com>
2024-12-30 17:06:34 +0530

    30-Dec-2024

---
+++ grepExes.sh
@@ -0,0 +1,17 @@
+grep "sales" emp1.lst
+echo ""
+grep -v "sales" emp1.lst
+echo ""
+grep "sal.s" emp1.lst
+echo ""
+grep "^3" emp1.lst
+echo ""
+grep "ba.un" emp1.lst
+echo ""
+grep "sh" emp1.lst
+echo ""
+egrep "5000$" emp1.lst
+echo ""
+egrep "^12" emp1.lst
+echo ''
+grep -i "SHYAM" emp1.lst
