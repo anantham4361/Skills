f2d39665eb8259474d9b1d290bbf6418a02936d1
anantham4361 <anantham4361@gmail.com>
2025-01-02 17:19:15 +0530

    Modified python file

--- first.py
+++ first.py
@@ -1,1 +1,23 @@
-print("This python program uploaded to githu using CLI")
+#print("This python program uploaded to github using CLI")
+"""
+#assigning multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+"""
+"""
+#assign the same value to multiple variables 
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+"""
+"""
+#extract the values into variables
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+"""
